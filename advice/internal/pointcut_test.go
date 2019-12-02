67ad79e8db8c3e1d317e59444d7633ee25bb2631
Iván Corrales Solera <developer@wesovilabs.com>
2019-12-02 18:15:36 +0100

    test: adding new tests for advice expressions

--- advice/internal/pointcut_test.go
+++ advice/internal/pointcut_test.go
@@ -16,4 +16,13 @@ func Test_NormalizePointcut(t *testing.T) {
 	assert.False(expr.Match([]byte("pkg.Object.fn(string,*int)")))
 	assert.False(expr.Match([]byte("pkg.Person?.fn(string,*int)")))
 
+
+	expr = NormalizePointcut("pkg.*?.fn(...)...")
+	fmt.Println(expr.String())
+	assert.True(expr.Match([]byte("pkg.Person.fn(string,*int)")))
+	assert.True(expr.Match([]byte("pkg.fn(string,*int)")))
+	assert.True(expr.Match([]byte("pkg.fn()")))
+	assert.True(expr.Match([]byte("pkg.Object.fn(string,*int)")))
+	assert.False(expr.Match([]byte("pkg.Person?.fn(string,*int)")))
+
 }
